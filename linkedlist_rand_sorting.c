1d6dd9261b941527a3e4f953fad66d46681d1385
01fe21bec088 <105053373+01fe21bec088@users.noreply.github.com>
2023-05-06 12:10:20 +0530

    random number sorting using linked list

---
+++ linkedlist_rand_sorting.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+struct NODE
+{
+    int data;
+    struct NODE *next;
+};
+typedef struct NODE *node;
+node head=NULL;
+node tail=NULL;
+node tail1=NULL;
+node insert(int c)
+{
+    node newnode=(node)malloc(sizeof(struct NODE));
+    if(newnode==NULL)
+    {
+        printf("\nNode not created");
+        exit(0);
+    }
+    newnode->data=c;
+    newnode->next=NULL;
+    return newnode;
+}
+void read(FILE *fp)
+{
+    int c; node cur;
+    int n;
+    printf("\nEnter range: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        fscanf(fp,"%d",&c);
+        if(i==0)
+        {
+            head=insert(c);
+            tail=head;
+        }
+        else
+        {
+            tail1=insert(c);
+            //sort();
+            tail->next=tail1;
+            tail=tail->next;
+        }
+    }
+}
+void disp()
+{
+    FILE *fp;
+    fp=fopen("LL.txt","w");
+    if(head==NULL)
+    {
+        printf("\nEmpty..");
+        return ;
+    }
+    node temp=head;
+    while(temp!=NULL)
+    {
+        fprintf(fp,"%d  ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+void rad(FILE *fp)
+{
+    for(int i=0;i<100;i++)
+    {
+        fprintf(fp,"%d ",rand()%100);
+    }
+}
+void sort()
+{
+    node cur=head,temp=NULL,temp1=head;
+    while(cur->next!=NULL)
+    {
+        while(temp1->next!=NULL)
+        {
+            if(temp1->data>temp1->next->data)
+            {
+                int temp=temp1->data;
+                temp1->data=temp1->next->data;
+                temp1->next->data=temp;
+            }
+            temp1=temp1->next;
+        }
+        cur=cur->next;
+        temp1=head;
+    }
+    //temp=cur;
+}
+int main()
+{
+    FILE *fp;
+    fp=fopen("LL.txt","w");
+    if(fp==NULL)
+    {
+        printf("\nFile not created.");
+        exit(0);
+    }
+    rad(fp);
+    fclose(fp);
+    fp=fopen("LL.txt","r");
+    read(fp);
+    sort();
+    disp();
+    fclose(fp);
+    return 0;
+}
