ce757baf0deaa88690fbcd48e1d44d9eb8b40509
01fe21bec088 <105053373+01fe21bec088@users.noreply.github.com>
2023-05-06 10:38:53 +0530

    random number generation

---
+++ random_number_generator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp1;
+    int n,i;
+    fp1=fopen("ran.txt","w");
+    printf("elements: ");
+    scanf("%d",&n);
+
+    int r[n];
+    for(i=0;i<n;i++)
+    {
+        r[i]=rand();
+        printf("\n%d",r[i]);
+        fprintf(fp1,"%d\n",r[i]);
+    }
+    fclose(fp1);
+}
